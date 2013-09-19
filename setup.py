9ff939204986abe2db4d4a0378593a1af8405d60
Mart <mart@martlubbers.net>
2013-09-19 12:59:17 +0200

    Updated install script

--- setup.py
+++ setup.py
@@ -2,12 +2,10 @@
 
 from distutils.core import setup
 
-setup(
-		name='pympi', 
+setup(	name='pympi', 
 		version='0.5', 
 		description='Python Linguistic Modules',
 		author='Mart Lubbers',
 		author_email='mart@martlubbers.net',
 		url='https://github.com/dopefishh/pympi',
-		py_modules=['src/Elan', 'src/Praat']
-	 )
+		py_modules=['src/Elan', 'src/Praat'])
