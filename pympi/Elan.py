c4f5d091fa062adf995ce3a1b1337734fed7dbb8
dopefishh <mart@martlubbers.net>
2014-02-17 09:38:54 +0100

    Linguistic type fix

--- pympi/Elan.py
+++ pympi/Elan.py
@@ -119,6 +119,9 @@ class Eaf:
 
 	def addTier(self, tierId, ling='default-lt', parent=None, locale=None, part=None, ann=None, tierDict=None):
 		"""Adds a tier giving a id and type and optional extra data"""
+		if ling not in linguistic_types:
+			warnings.warn('addTier: Linguistic type non existent, choosing the first')
+			ling = linguistic_types.keys()[0]
 		if tierDict is None:
 			self.tiers[tierId] = ({}, {}, {'TIER_ID':tierId, 'LINGUISTIC_TYPE_REF':ling, 'PARENT_REF':parent, 'PARTICIPANT':part, 'DEFAULT_LOCALE':locale, 'ANNOTATOR':ann}, len(self.tiers))
 		else:
