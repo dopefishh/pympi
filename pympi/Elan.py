f7ee43dff7e809395bd097849d2e7bc6e602096b
hadware <hadwarez@gmail.com>
2019-05-21 15:40:37 +0200

    Fixing the extract function

--- pympi/Elan.py
+++ pympi/Elan.py
@@ -475,7 +475,7 @@ class Eaf:
         for t in eaf_out.get_tier_names():
             for ab, ae, value in eaf_out.get_annotation_data_for_tier(t):
                 if ab > end or ae < start:
-                    eaf_out.remove_annotation(t, (start-end)//2, False)
+                    eaf_out.remove_annotation(t, (ae - ab) // 2, False)
         eaf_out.clean_time_slots()
         return eaf_out
 
